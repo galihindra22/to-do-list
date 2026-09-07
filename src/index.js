38ac624c453feba8874aa078627c1261b13a4a71
galihindra22 <galihindranevano@gmail.com>
2026-09-07 11:24:59 +0700

    Set the webpack setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+
+class ToDos{
+    constructor(){
+        this.title = title,
+        this.description = description,
+        this.dueDate = dueDate,
+        this.priority = priority,
+        this.note = note,
+        this.checklist = checklist
+    }
+
+}
